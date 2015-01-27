ef3276d1cba242747058a097df4edefcfb716800
Dennis Kreminsky <etranger@etranger.ru>
2015-01-27 22:09:46 +0200

    typo fixes

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 /etc/init.d/postgresql start
 cd /oskari/oskari-server/standalone-jetty
-mvn clean install exec:java -Ddb.username=postgres -Ddb.password=postgres"
+mvn exec:java -Ddb.username=postgres -Ddb.password=postgres
+
