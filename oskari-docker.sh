28e43049a6991a803a1dfcb7aa000d131cfbdaee
Dennis Kreminsky <etranger@etranger.ru>
2015-01-27 22:13:14 +0200

    minor fixes to launch code

--- oskari-docker.sh
+++ oskari-docker.sh
@@ -2,7 +2,7 @@
 
 base="ubuntu:latest"
 image="okffi/oskari"
-oskaricmd="run.sh"
+oskaricmd="./run.sh"
 
 # TODO: add persistent data container for postgres separate from the code container http://stackoverflow.com/questions/18496940/how-to-deal-with-persistent-storage-e-g-databases-in-docker
 
